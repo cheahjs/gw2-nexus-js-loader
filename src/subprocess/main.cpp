7fdad115519d40fb254df61daae5ea725c74fab5
Jun Siang Cheah <git@jscheah.me>
2026-02-12 15:57:05 +0800

    Add /Od debug optimization to CEF host and stderr logging to subprocess
    Disable optimizations (/Od) for the CEF host build to aid debugging. Add
    diagnostic stderr logging to the subprocess, redirecting to a log file with
    PID and command line info for troubleshooting CEF child process launches.

--- src/subprocess/main.cpp
+++ src/subprocess/main.cpp
@@ -1,4 +1,6 @@
 #include <windows.h>
+#include <cstdio>
+#include <string>
 #include "include/cef_app.h"
 #include "renderer_app.h"
 
@@ -8,6 +10,24 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
                       HINSTANCE /*hPrevInstance*/,
                       LPWSTR /*lpCmdLine*/,
                       int /*nCmdShow*/) {
+    auto ResolveModuleDir = []() -> std::string {
+        char path[MAX_PATH] = {};
+        GetModuleFileNameA(nullptr, path, MAX_PATH);
+        std::string fullPath(path);
+        size_t pos = fullPath.find_last_of("\\/");
+        if (pos == std::string::npos) {
+            return ".";
+        }
+        return fullPath.substr(0, pos);
+    };
+
+    std::string stderrPath = ResolveModuleDir() + "\\cef_subprocess_stderr.log";
+    freopen(stderrPath.c_str(), "a", stderr);
+    fprintf(stderr, "\n[CEF Subprocess] ===========================================\n");
+    fprintf(stderr, "[CEF Subprocess] PID: %lu\n", GetCurrentProcessId());
+    fprintf(stderr, "[CEF Subprocess] Command line: %s\n", GetCommandLineA());
+    fflush(stderr);
+
     CefMainArgs mainArgs(hInstance);
 
     CefRefPtr<RendererApp> app = new RendererApp();
@@ -15,5 +35,7 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
     // CefExecuteProcess returns -1 for the browser process (shouldn't happen here)
     // and the exit code for sub-processes.
     int exitCode = CefExecuteProcess(mainArgs, app, nullptr);
+    fprintf(stderr, "[CEF Subprocess] CefExecuteProcess returned: %d\n", exitCode);
+    fflush(stderr);
     return exitCode;
 }
